1766ae96ff25334d77dd6c74f63813b858b8e0ad
JISUN <JISUN@DESKTOP-703LGDE>
2018-10-11 10:01:13 +0900

    MemberService 메소드 추가

--- inssaground/src/main/java/org/kosta/inssaground/model/service/MemberService.java
+++ inssaground/src/main/java/org/kosta/inssaground/model/service/MemberService.java
@@ -1,6 +1,9 @@
 package org.kosta.inssaground.model.service;
 
+import org.kosta.inssaground.model.vo.GroundVO;
+import org.kosta.inssaground.model.vo.ListVO;
 import org.kosta.inssaground.model.vo.MemberVO;
+import org.kosta.inssaground.model.vo.ScheduleVO;
 
 public interface MemberService {
 	public boolean checkEmailKey(String id,String emailKey);
@@ -9,6 +12,6 @@ public interface MemberService {
 	public MemberVO findMemberById(String id);
 	public void updateMember(MemberVO vo);
 	public void withdrawMember(MemberVO vo);
-//	public ListVO myGroundList(String id); // 내가 참여한 모임 보기
-//	public ListVO myScheduleList(String id); // 내가 참여한 모임의 일정 보기
+	public ListVO<GroundVO> myGroundList(String id); // 내가 참여한 모임 보기
+	public ListVO<ScheduleVO> myScheduleList(String id); // 내가 참여한 모임의 일정 보기
 }
