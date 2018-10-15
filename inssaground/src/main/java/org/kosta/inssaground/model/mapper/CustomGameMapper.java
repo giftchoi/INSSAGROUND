e5ef2f609fa9e9c65cb380da10349a38405c5f7f
moonblin <mjw0613@gmail.com>
2018-10-15 19:27:34 +0900

    CustomGameMapper, GameServiceImpl 충돌남

--- inssaground/src/main/java/org/kosta/inssaground/model/mapper/CustomGameMapper.java
+++ inssaground/src/main/java/org/kosta/inssaground/model/mapper/CustomGameMapper.java
@@ -1,8 +1,11 @@
 package org.kosta.inssaground.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.kosta.inssaground.model.vo.CustomGameVO;
 
 @Mapper
 public interface CustomGameMapper {
+	public int getTotalCustomGameCount();
 
+	public CustomGameVO getCustomGameList();
 }
